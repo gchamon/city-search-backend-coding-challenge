7ae0b096703201a6f5e9e9c7802a001db2d94d26
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2024-02-18 10:14:33 -0300

    implement query_name test

--- backend.py
+++ backend.py
@@ -5,7 +5,15 @@ def load_catalog(catalog_filename):
         catalog = csv.DictReader(catalog_fp, delimiter="\t", quoting=csv.QUOTE_NONE)
         return list(catalog)
 
+def query_name(catalog, query):
+    return []
+
 if __name__ == "__main__":
     canada_catalog = load_catalog("data/cities_canada-usa.tsv")
     print(canada_catalog[0])
     print(canada_catalog[-1])
+    kirks = (city for city in canada_catalog if "Kirk" in city["name"])
+    import json
+    print(json.dumps([{"name": k["name"], "lat": k["lat"], "long": k["long"]} for k in kirks], indent=2))
+
+
--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import unittest
+
 import backend
+
 
 class TestCatalogLoad(unittest.TestCase):
     def test_load_catalog(self):
@@ -8,11 +10,43 @@ class TestCatalogLoad(unittest.TestCase):
             catalog_lines = len(catalog_fp.readlines())
         self.assertEquals(len(catalog), catalog_lines - 1)
 
-# class TestCitySearch(unittest.TestCase):
-#
-#     def test_name_query(self):
-#         query = "Londo"
-#
+
+class TestCitySearch(unittest.TestCase):
+    def setUp(self):
+        self.catalog = backend.load_catalog("data/cities_canada-usa.tsv")
+
+    def test_name_query(self):
+        query = "Kirk"
+        results = backend.query_name(catalog=self.catalog, query=query)
+        expected = [
+            {
+                "name": "Kirkland",
+                "lat": "45.45008",
+                "long": "-73.86586"
+            },
+            {
+                "name": "Kirkland Lake",
+                "lat": "48.14461",
+                "long": "-80.03767"
+            },
+            {
+                "name": "Kirkwood",
+                "lat": "38.58339",
+                "long": "-90.40678"
+            },
+            {
+                "name": "Kirksville",
+                "lat": "40.19475",
+                "long": "-92.58325"
+            },
+            {
+                "name": "Kirkland",
+                "lat": "47.68149",
+                "long": "-122.20874"
+            }
+        ]
+        self.assertEquals(results, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
