a0134c4d39308762ebd2919be8abbd06bb2c5eda
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2024-02-18 10:04:25 -0300

    load catalog with DictReader

--- backend.py
+++ backend.py
@@ -1,2 +1,11 @@
+import csv
+
 def load_catalog(catalog_filename):
-    return []
+    with open(catalog_filename) as catalog_fp:
+        catalog = csv.DictReader(catalog_fp, delimiter="\t", quoting=csv.QUOTE_NONE)
+        return list(catalog)
+
+if __name__ == "__main__":
+    canada_catalog = load_catalog("data/cities_canada-usa.tsv")
+    print(canada_catalog[0])
+    print(canada_catalog[-1])
--- test.py
+++ test.py
@@ -6,7 +6,7 @@ class TestCatalogLoad(unittest.TestCase):
         catalog = backend.load_catalog("data/cities_canada-usa.tsv")
         with open("data/cities_canada-usa.tsv") as catalog_fp:
             catalog_lines = len(catalog_fp.readlines())
-        self.assertEquals(len(catalog), catalog_lines - 2)
+        self.assertEquals(len(catalog), catalog_lines - 1)
 
 # class TestCitySearch(unittest.TestCase):
 #
