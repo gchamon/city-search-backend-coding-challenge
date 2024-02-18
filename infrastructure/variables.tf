93c48c0053098d38ff2bcff8557115562fd70bc0
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2024-02-18 14:53:08 -0300

    add iac to create route53 zone and certificate

---
+++ infrastructure/variables.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+  description = "AWS Region"
+  default = "us-east-2"
+}
+
+variable "delegation_set_id" {
+  description = "Reusable delegation set id for Route53 zone creation"
+}
+
+variable "route53_zone" {
+  description = "Route53 zone name"
+}
